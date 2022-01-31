832b4e21c06d091ba2560afa403e500796a0d059
Carlos Ramirez <ramc.rc@gmail.com>
2022-01-31 11:20:23 -0600

    add port

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.get("/", function(req, res){
     res.render("home");
 });
 
-app.listen(3000, function() {
-    console.log("Server has started successfully");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server has started successfully");
 });
